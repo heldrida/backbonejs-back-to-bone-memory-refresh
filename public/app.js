f018d3db2edd6a059e65e94834e3f5cab0f56e13
Helder <heldrida@gmail.com>
2017-09-11 22:47:00 +0100

    created some templates; and typed in some code for the blogs views

--- public/app.js
+++ public/app.js
@@ -28,3 +28,35 @@ const blog2 = new Blog({
 // instance collection
 
 const blogs = new Blogs([blog1, blog2])
+
+// Backbone views
+
+// Backbone view for one blog
+
+const BlogView = Backbone.View.extend({
+  model: new Blog(),
+  tagName: 'tr',
+  initialize: function () {
+    this.template = _.template($('.blogs-list-template').html())
+  },
+  render: function () {
+    this.$el.html(this.template(this.model.toJSON()))
+  }
+})
+
+// Backbone view for all blogs
+
+const BlogsView = Backbone.View.extend({
+  model: blogs,
+  el: $('.blogs-list'),
+  initialize: function () {
+    this.model.on('add', this.render(), this)
+  },
+  render: function () {
+    var self = this
+    this.$el.html('')
+    _.each(this.model.toArray(), function (blog) {
+      this.$el.append((new BlogView({model: blog})).render().$el)
+    })
+  }
+})
